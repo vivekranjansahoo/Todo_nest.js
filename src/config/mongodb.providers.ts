38f16e17b2f9d61cf44e1dc9a72bb726a0ecd85c
Vivek Ranjan Sahoo <vivekranjansahoo81@gmail.com>
2024-08-19 14:01:25 +0530

    update mongo url

--- src/config/mongodb.providers.ts
+++ src/config/mongodb.providers.ts
@@ -7,7 +7,11 @@ export const MongodbConfig = [
   {
     provide: DATABASE_CONNECTION,
     useFactory: async (): Promise<Db> => {
-      const client = new MongoClient('mongodb://localhost:27017');
+      // const client = new MongoClient('mongodb://localhost:27017');
+
+      const client = new MongoClient(
+        'mongodb+srv://vivekranjansahoo81:nestjs@cluster0.myewz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+      );
       await client.connect();
       return client.db(DATABASE_NAME);
     },
